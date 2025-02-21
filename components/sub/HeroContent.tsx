f7b3c83cbcdbc2372ad3705ba8955dde727086d6
kavinraj7305 <kavinrajnandakumar@gmail.com>
2025-02-21 11:13:28 +0530

    collapse imgae

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -13,13 +13,13 @@ const HeroContent = () => {
     animate="visible"
     className='flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]'
     >
-      <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
+      <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start xs:mr-10'>
         <motion.div
         variants={slideInFromTop}
-        className='Welcome-box py-[15px] px-[7px] border border-[#7042f88b] opacity-[0.9]'
+        className='Welcome-box py-[15px] px-[7px] border border-[#7042f88b] opacity-[0.9] '
         >
-            <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-6'/>
-            <h1 className='welcome-text text-[13px] text-white pr-3 '>
+            <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-6 xs:h-10 xs:w-24 xs:mr-0 '/>
+            <h1 className='welcome-text text-[13px] xs:text-[20px] text-white pr-3 '>
                 RIT STARTUP
             </h1>
         </motion.div> 
@@ -35,7 +35,7 @@ const HeroContent = () => {
         </motion.div>
         <motion.p
         variants={slideInFromLeft(0.8)}
-        className='text-lg text-gray-400 my-5 max-w-[600px]'
+        className='text-lg text-gray-400 my-5 max-w-[600px] '
         >
           An interactive platform for our college startup club, connecting innovators, providing resources, and fostering collaboration through discussions, project showcases, and networking opportunities.
         </motion.p>
