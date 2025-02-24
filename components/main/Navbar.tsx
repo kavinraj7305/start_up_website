e171a19ae8c3f4cdfb4104604d2ec7cdfea2eb4b
kavinraj7305 <kavinrajnandakumar@gmail.com>
2025-02-25 01:03:40 +0530

    changes the chatbot to responsive

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -58,7 +58,7 @@ const Navbar = () => {
     <Drawer.Portal>
       <Drawer.Overlay className="fixed inset-0 " />
       <Drawer.Content
-        className="right-2 top-2 bottom-2 fixed z-[9999] outline-none w-[500px] xs:w-[350px] flex"
+        className="right-2 top-2 bottom-2 fixed z-[9999] outline-none w-[500px] xs:w-[350px] sm:w-[400px] md:w-[450px] lg:w-[500px] flex"
         style={{ '--initial-transform': 'calc(100% + 8px)' } as React.CSSProperties}
       >
         <div className="bg-white/30 backdrop-blur-lg h-full w-full grow p-5 flex flex-col rounded-[16px] ">
