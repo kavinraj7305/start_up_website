21cfdaf80efb8aa5f60ff8e76db967c77216a9f9
kavinraj7305 <kavinrajnandakumar@gmail.com>
2025-02-25 21:43:36 +0530

    more changes ,checking for phase

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <div className="flex flex-col gap-1
       ">
         <Hero/>
-        <div className="xs:hidden md:block">
+        <div className="">
         <Encryption/>
         <Skills/>
         </div>
